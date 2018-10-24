6b091722729b5e1171af83aea5ea328c6e55e604
Chris Hanson <BlondyHanson@gmail.com>
2018-10-24 16:39:01 +0100

    Final version

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 $('select').on('change',function() {
   $('.container').empty();
   var url = 'https://api.nytimes.com/svc/topstories/v2/' + $('select').val() + '.json';
@@ -8,16 +10,24 @@ $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
-  for (let i=0; i < 12; i++) {
+  let i = 0;
+  let AddArticle = 0;
+
+  for (i=0; i < result.results.length && AddArticle < 12; i++) {
+  let currentResult = result.results[i];
+  if (currentResult.multimedia.length !== 0) {
+    AddArticle++;
   let title = result.results[i].abstract;
   let articleUrl = result.results[i].url;
   let imageUrl = result.results[i].multimedia[4].url;
 
   $('title').append(result.results[i].abstract);
   $('.container').append('<div class="articlecontainer"><a href="'+ articleUrl +'" class="link"><h1 class="summary">'+ title +'</h1> </a></div>');
-  $('.articleContainer').last().css('background-image', 'url(' + imageUrl+')');  
+  $('.articleContainer').last().css('background-image', 'url(' + imageUrl+')');
+}
 }
 }).fail(function(err) {
   throw err;
+});
 });
 });
