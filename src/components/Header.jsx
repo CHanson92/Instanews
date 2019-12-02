eb506b35e85ac6d57b80950b03b614479b7208b2
Chris Hanson <BlondyHanson@gmail.com>
2019-12-02 17:49:45 +0000

    changed option to have capitalized first letter and modified eslint-prettier

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import logo from '../assets/images/nyt-logo.svg'
+import React from 'react';
+import logo from '../assets/images/nyt-logo.svg';
 
 const Header = props => {
-    let filter = props.filter
-    let onChange = props.onChange()
+    let filter = props.filter;
+    let onChange = props.onChange();
     return (
         <header>
             <div>
@@ -17,13 +17,13 @@ const Header = props => {
                     </option>
                     {filter.map((i, index) => (
                         <option key={index} value={i}>
-                            {i}
+                            {i.charAt(0).toUpperCase() + i.slice(1)}
                         </option>
                     ))}
                 </select>
             </section>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
